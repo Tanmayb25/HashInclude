eb4d717ffa7a63786ddc32a6913c37228f7feaa4
tanishqc16 <tanishqbc16@gmail.com>
2023-02-16 20:12:24 +0530

    questionnaire

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import {app} from './firebase'
 import SignUp from './pages/SignUp';
+import Questionnaire from './pages/questionnaire';
 
 
 function App() {
   return (
     <>
-      <SignUp/>
+      {/* <SignUp/> */}
+      <Questionnaire/>
     </>
   );
 }
