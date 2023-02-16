567dfcea423df2067861947d1fca99303673bd3c
Maruf Khan <maruf1611khan@gmail.com>
2023-02-16 13:59:46 +0530

    sign in page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
-    <h1>Hello World!!!</h1>
+    <SignIn/>
   );
 }
 
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <button className="signInbtn">Google Sign in</button>
+  )
+}
+
+export default SignIn
