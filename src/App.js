aac6621723ee8c41af8517c7ca174aa676748d9e
Maruf Khan <maruf1611khan@gmail.com>
2023-02-16 14:33:46 +0530

    google sign in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {app} from './firebase'
 import SignIn from './pages/SignIn';
 
 function App() {
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {app} from '../firebase';
+
+
+import {getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider();
+googleProvider.addScope('https://www.googleapis.com/auth/calendar.events.readonly');
 
 function SignIn() {
+
+  const signInWithGoogle = ()=> {
+    signInWithPopup(auth, googleProvider);
+  }
+
+  
   return (
-    <button className="signInbtn">Google Sign in</button>
+    <button onClick={signInWithGoogle} className="signInbtn">Google Sign in</button>
   )
 }
 
