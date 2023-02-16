b486482ef014a240a16ed8bffbb0ab26f20d0887
Maruf Khan <maruf1611khan@gmail.com>
2023-02-16 13:10:24 +0530

    init firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUcv5JyRK2EzytjXJvV2C7IH8EFXqpuP8",
+    authDomain: "hashinclude-6b156.firebaseapp.com",
+    projectId: "hashinclude-6b156",
+    storageBucket: "hashinclude-6b156.appspot.com",
+    messagingSenderId: "8105056541",
+    appId: "1:8105056541:web:836ce05d4532dba2dda9f4"
+  };
+
+  export const app = initializeApp(firebaseConfig);
